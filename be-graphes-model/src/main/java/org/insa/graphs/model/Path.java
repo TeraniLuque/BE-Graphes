b758f56409f78b0d978f69de74b923737df13584
Luque Terani <luque@srv-neo2>
2020-03-20 18:13:10 +0100

    Message

--- be-graphes-model/src/main/java/org/insa/graphs/model/Path.java
+++ be-graphes-model/src/main/java/org/insa/graphs/model/Path.java
@@ -210,11 +210,15 @@ public class Path {
      * 
      * @return Total length of the path (in meters).
      * 
-     * @deprecated Need to be implemented.
+     * Need to be implemented.
      */
     public float getLength() {
-        // TODO:
-        return 0;
+        float longueur = 0.0f;
+        for(Arc a : this.arcs) {
+        	longueur += a.getLength();
+        }
+    	
+        return longueur;
     }
 
     /**
@@ -225,11 +229,11 @@ public class Path {
      * @return Time (in seconds) required to travel this path at the given speed (in
      *         kilometers-per-hour).
      * 
-     * @deprecated Need to be implemented.
+     * Need to be implemented.
      */
     public double getTravelTime(double speed) {
-        // TODO:
-        return 0;
+        
+        return (this.getLenght()/(float)speed);
     }
 
     /**
